f094ca2bc65b477595099c2c4f2f47292ed2eb80
Nándor Holozsnyák <nandor.holozsnyak@gmail.com>
2023-01-04 22:27:03 +0100

    chore(docs): JBang help script is generating all available formats

--- samples/jbang-project/generate-documentation.sh
+++ samples/jbang-project/generate-documentation.sh
@@ -7,11 +7,37 @@ echo "Jar file location:${JAR_LOCATION}"
 ${JBANG_EXECUTABLE} ../../jbang/src/PropertyDocumenter.java generate \
                            -n "Simple JBang Application" -i ${JAR_LOCATION} \
                            -tt HTML \
-                           -o spring-app-simple-property-docs.html
+                           -o target/spring-app-simple-property-docs.html
+
+${JBANG_EXECUTABLE} ../../jbang/src/PropertyDocumenter.java generate \
+                           -n "Simple JBang Application" -i ${JAR_LOCATION} \
+                           -tt ADOC \
+                           -o target/spring-app-simple-property-docs.adoc
+
+${JBANG_EXECUTABLE} ../../jbang/src/PropertyDocumenter.java generate \
+                           -n "Simple JBang Application" -i ${JAR_LOCATION} \
+                           -tt MARKDOWN \
+                           -o target/spring-app-simple-property-docs.md
+
+
+${JBANG_EXECUTABLE} ../../jbang/src/PropertyDocumenter.java aggregate \
+                           -n "Simple JBang Application" \
+                           -mn "Spring Application example 1" -i ${JAR_LOCATION} \
+                           -mn "Spring Application example 2" -i ${JAR_LOCATION} \
+                           -tt ADOC \
+                           -o target/spring-app-aggregated-property-docs.adoc
+
+
+${JBANG_EXECUTABLE} ../../jbang/src/PropertyDocumenter.java aggregate \
+                           -n "Simple JBang Application" \
+                           -mn "Spring Application example 1" -i ${JAR_LOCATION} \
+                           -mn "Spring Application example 2" -i ${JAR_LOCATION} \
+                           -tt MARKDOWN \
+                           -o target/spring-app-aggregated-property-docs.md
 
 ${JBANG_EXECUTABLE} ../../jbang/src/PropertyDocumenter.java aggregate \
                            -n "Simple JBang Application" \
                            -mn "Spring Application example 1" -i ${JAR_LOCATION} \
                            -mn "Spring Application example 2" -i ${JAR_LOCATION} \
                            -tt HTML \
-                           -o spring-app-aggregated-property-docs.html
+                           -o target/spring-app-aggregated-property-docs.html
